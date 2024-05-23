3bd1685fe0f32d21fa46356703e8d95b8bf63bde
TANK8K <tank8kk@gmail.com>
2024-05-24 06:37:37 +0800

    Improve the website

---
+++ scripts/toggle_password.js
@@ -0,0 +1,8 @@
+function toggle_password() {
+  var x = document.getElementById("password");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
---
+++ scripts/validate.js
@@ -0,0 +1,71 @@
+let loginForm = document.getElementById("loginForm");
+
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
+  let username = document.getElementById("username").value;
+
+  if (email == "" || password == "" || username == "") {
+    alert("Ensure you input a value in all fields!");
+  } else {
+    validate()
+  }
+});
+
+async function validate() {
+  let obj;
+  const res = await fetch(
+    "https://dev-nakama.winterpixel.io/v2/account/authenticate/email?create=false",
+    {
+      method: "POST",
+      headers: {
+        authorization: "Basic OTAyaXViZGFmOWgyZTlocXBldzBmYjlhZWIzOTo=",
+      },
+      body: JSON.stringify({
+        email: document.getElementById("email").value,
+        password: document.getElementById("password").value,
+        vars: { client_version: "99999" },
+      }),
+    }
+  );
+  obj = await res.json();
+  status_code = res.status;
+  console.log(obj);
+  token = obj["token"];
+  console.log(token);
+
+
+  if (status_code == 200) {
+    email = document.getElementById("email").value;
+    password = document.getElementById("password").value;
+    username = document.getElementById("username").value;
+
+    console.log(token)
+    console.log(username)
+    const res2 = await fetch(
+      "https://dev-nakama.winterpixel.io/v2/account",
+      {
+        method: "PUT",
+        headers: {
+          authorization: "Bearer " + token,
+        },
+        body: JSON.stringify({
+          display_name: username,
+        }),
+      }
+    );
+
+    if (document.getElementById('remember').checked) {
+      setCookie("email", email, 9999)
+      setCookie("password", password, 9999)
+    }
+
+    alert("Success!");
+  } else if (status_code == 401) {
+    alert("Invalid credentials.");
+  } else if (status_code == 404) {
+    alert("User account not found.");
+  }
+}
